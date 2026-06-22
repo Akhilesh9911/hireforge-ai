6213d8548bba394e2f70e96bc7ec5b75b628c084
Akhilesh Chitare <akhileshchitare04@gmail.com>
2026-06-22 16:31:38 +0530

    Initial commit: Spring Boot project setup with Spring Web, JPA, MySQL, Validation, DevTools

---
+++ src/main/java/com/hireforge/hireforge_ai/HireforgeAiApplication.java
@@ -0,0 +1,13 @@
+package com.hireforge.hireforge_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireforgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireforgeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hireforge/hireforge_ai/HireforgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireforge.hireforge_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireforgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
